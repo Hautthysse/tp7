4cf7518ce12240f721e6daea55f213145127a11c
Gilles Subrenat <7287005-subrenat@users.noreply.gitlab.com>
2022-12-23 11:01:54 +0100

    section 4.3 : action Injection::deux

--- src/Controller/Sandbox/InjectionController.php
+++ src/Controller/Sandbox/InjectionController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Sandbox;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/sandbox/injection', name: 'sandbox_injection')]
@@ -17,6 +18,21 @@ class InjectionController extends AbstractController
         dump($request->query->get('foo'));
         dump($request->query->all());
         dump($request->cookies->all());
+        return new Response('<body>Injection::un</body>');
+    }
+
+    #[Route('/deux', name: '_deux')]
+    public function deuxAction(Request $request, Session $session): Response
+    {
+        // note : on peut aussi récupérer la session avec : $request->getSession();
+        if ($request->query->get('compteur') !== null)
+            $session->set('compteur', $request->query->get('compteur'));
+        elseif ($request->query->get('inc') !== null)
+            $session->set('compteur', $session->get('compteur') + 1);
+        elseif ($request->query->get('supp') !== null)
+            $session->remove('compteur');
+        dump($session->all());
+        dump($_SESSION);
         return new Response('<body>Injection::un</body>');
     }
 }
