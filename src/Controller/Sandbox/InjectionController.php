79ee65fc2645e876d79af299f2ff8d61991992b4
Gilles Subrenat <7287005-subrenat@users.noreply.gitlab.com>
2022-12-23 08:08:55 +0100

    section 4.1 : contrôleur Injection (vide)

---
+++ src/Controller/Sandbox/InjectionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller\Sandbox;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sandbox/injection', name: 'sandbox_injection')]
+class InjectionController extends AbstractController
+{
+}
