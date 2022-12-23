8d2d24814e01cca8e36fc18d2a68487d299e24b2
Gilles Subrenat <7287005-subrenat@users.noreply.gitlab.com>
2022-12-23 09:09:47 +0100

    section 4.2 : ajout action Injection::un qui récupère l'objet Request

--- src/Controller/Sandbox/InjectionController.php
+++ src/Controller/Sandbox/InjectionController.php
@@ -3,10 +3,17 @@
 namespace App\Controller\Sandbox;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/sandbox/injection', name: 'sandbox_injection')]
 class InjectionController extends AbstractController
 {
+    #[Route('/un', name: '_un')]
+    public function unAction(Request $request): Response
+    {
+        dump($request);
+        return new Response('<body>Injection::un</body>');
+    }
 }
