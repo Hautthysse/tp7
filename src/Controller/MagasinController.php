3ae4fd0733fc976d2aace347aa2c1c4b3fca3d5b
Gilles Subrenat <7287005-subrenat@users.noreply.gitlab.com>
2022-12-25 09:33:50 +0100

    section 8.5 : action Magasin::stock

--- src/Controller/MagasinController.php
+++ src/Controller/MagasinController.php
@@ -24,4 +24,34 @@ class MagasinController extends AbstractController
         );
         return $this->render('Magasin/valeurStock.html.twig', $args);
     }
+
+    #[Route(
+        '/stock/{id}/{valinf}/{valsup}',
+        name: '_stock',
+        requirements: [
+            'id' => '[1-9]\d*',
+            'valinf' => '0|[1-9]\d*',
+            'valsup' => '-1|0|[1-9]\d*',
+        ],
+        defaults: [
+            'valinf' => 0,
+            'valsup' => -1,
+        ],
+    )]
+    public function stockAction(int $id, int $valinf, int $valsup): Response
+    {
+        // liste fictive en attendant l'interrogation de la base de données
+        $produits = array(
+            [ 'denomination' => 'marteau',   'quantite' => 37, 'prixUnitaire' => 22 ],
+            [ 'denomination' => 'tournevis', 'quantite' => 12, 'prixUnitaire' => 24 ],
+            [ 'denomination' => 'perçeuse',  'quantite' => 8,  'prixUnitaire' => 258 ],
+        );
+        $args = array(
+            'id' => $id,
+            'valinf' => $valinf,
+            'valsup' => $valsup,
+            'produits' => $produits,
+        );
+        return $this->render('Magasin/stock.html.twig', $args);
+    }
 }
