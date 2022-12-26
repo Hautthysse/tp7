944971a810f6ae5becabc721f8058243ea774dfb
Gilles Subrenat <7287005-subrenat@users.noreply.gitlab.com>
2022-12-26 11:10:14 +0100

    section 2.4 : Twig::vue1 et sa vue "vide"

--- src/Controller/Sandbox/TwigController.php
+++ src/Controller/Sandbox/TwigController.php
@@ -9,4 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/sandbox/twig', name: 'sandbox_twig')]
 class TwigController extends AbstractController
 {
+    #[Route(
+        '/vue1',
+        name: '_vue1'
+    )]
+    public function vue1Action(): Response
+    {
+        return $this->render('Sandbox/Twig/vue1.html.twig');
+    }
 }
