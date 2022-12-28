c6f23cd28db053176a7dd80faf7189fb8f801eb7
Gilles Subrenat <7287005-subrenat@users.noreply.gitlab.com>
2022-12-28 07:55:06 +0100

    section 4 : inclusion de contrôleur et action de test

--- src/Controller/Sandbox/TwigController.php
+++ src/Controller/Sandbox/TwigController.php
@@ -44,4 +44,24 @@ class TwigController extends AbstractController
     {
         return $this->render('Sandbox/Twig/vue4.html.twig');
     }
+
+    // surtout pas d'attribut Route
+    // note : cette action devrait être dans un contrôleur dédié
+    public function palmaresAction(int $n): Response
+    {
+        // normalement devrait être extrait de la base de données
+        $produits = [];
+        for ($i = 1; $i <= $n; $i ++)
+            $produits[] = ['denomination' => 'produit'.$i, 'prix' => 1000-$i];
+        return $this->render('Sandbox/Layouts/palmares.html.twig', [ 'produits' => $produits ]);
+    }
+
+    #[Route(
+        '/vue5',
+        name: '_vue5'
+    )]
+    public function vue5Action(): Response
+    {
+        return $this->render('Sandbox/Twig/vue5.html.twig');
+    }
 }
